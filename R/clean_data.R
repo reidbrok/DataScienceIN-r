9c5d6810b6d4b700a45c417e52b1304eb84b4ba2
reidbrok <yushuzou@gmail.com>
2023-09-18 11:27:38 -0400

    Initial commit

--- R/clean_data.R
+++ R/clean_data.R
@@ -9,3 +9,8 @@ clean_data <- rawdat %>% select(c("Country.Name", "X2000", "X2019")) %>%
   mutate(Year = as.numeric(Year))
 
 write.csv(clean_data, here("data","clean_data.csv"))
+
+
+use_git_config(user.name = "reidbrok", user.email = "yushuzou@gmail.com")
+use_git()
+use_github()
---
+++ R/clean_data_disaster.R
@@ -0,0 +1,17 @@
+library(tidyverse)
+library(here)
+library("usethis")
+
+raw_data <- read.csv(here("original","disaster.csv"))
+
+clean_data = raw_data %>% 
+  filter(Year <= 2019 & Year >= 2000 & Disaster.Type %in% c("Earthquake","Drought")) %>%
+  select(c("Year", "ISO", "Disaster.Type"))%>%
+  mutate(Drought = ifelse(Disaster.Type == "Drought",1,0),
+         Earthquake = ifelse(Disaster.Type == "Earthquake",1,0)) %>%
+  group_by(Year, ISO) %>%
+  summarize(Drought = max(Drought), Earthquake = max(Earthquake))
+
+use_git_config(user.name = "reidbrok",user.email = "yushuzou@gmail.com")
+use_git()
+use_github()
