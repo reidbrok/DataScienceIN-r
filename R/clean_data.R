8298baf33479bc9f1efd7dfd707ec297c91d4c72
reidbrok <yushuzou@gmail.com>
2023-09-18 11:00:23 -0400

    Initial commit

---
+++ R/clean_data.R
@@ -0,0 +1,11 @@
+library(tidyverse)
+library(here)
+library("usethis")
+
+rawdat <- read.csv(here("original","maternalmortality.csv"))
+
+clean_data <- rawdat %>% select(c("Country.Name", "X2000", "X2019")) %>% 
+  pivot_longer(c("X2000", "X2019"), names_to = "Year",names_prefix = "X", values_to = "MatMor") %>%
+  mutate(Year = as.numeric(Year))
+
+write.csv(clean_data, here("data","clean_data.csv"))
