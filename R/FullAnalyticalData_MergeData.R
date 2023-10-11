b543dc4d4c6f245b5c760130090cd9c722c05dcc
reidbrok <yushuzou@gmail.com>
2023-10-11 10:27:10 -0400

    Update the EDA plan

--- R/FullAnalyticalData_MergeData.R
+++ R/FullAnalyticalData_MergeData.R
@@ -1,17 +1,26 @@
 library(here)
 library(tidyverse)
 
-source(here("R","clean_data.R"))
-source(here("R","create_armed_conflict.R"))
+source(here("R","Mortality_Preprocess.R"))
+source(here("R","ArmedConflict_CreateVariable.R"))
 
-source(here("R","clean_data_disaster.R"))
+source(here("R","Disaster_Preprocess.R"))
 covariate = read.csv(here("original","covariates.csv"))
 
+# Filter the covariate
 covariate = covariate %>% filter(year <= 2019 & year >= 2000)
+# Check the ISO in disaster data
 disaster_data_cleaned %>% group_by(ISO) %>% summarise(n())
 
+# Rename the share column
 colnames(covariate)[4] = "Year"
 colnames(conflict)[1] = "Year"
+
+# Merge Data
 dataframe_list = list(covariate,disaster_data_cleaned,data_morality_wide,conflict)
 final_data = Reduce(left_join,dataframe_list)
-write.csv(final_data, "final_analytical_data.csv")
+
+# Output Final Data
+write.csv(final_data, here("data","final_analytical_data.csv"),row.names=FALSE)
+
+          
