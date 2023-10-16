b7be14839bf92b007220aa8b5b2f33c347549c6a
reidbrok <yushuzou@gmail.com>
2023-10-16 12:43:58 -0400

    Formating the table and figures

--- R/TABLE1. Demographic Table .R
+++ R/TABLE1. Demographic Table .R
@@ -2,7 +2,7 @@
 library(tidyverse)
 library(here)
 library(table1)
-
+library(kableExtra)
 #### Reading Data
 data <- read.csv(here("data","final_analytical_data.csv"))
 data <- data %>% filter(Year == 2000) ## Create baseline data
@@ -38,10 +38,10 @@ data$OECD<- as.factor(data$OECD)
 rndr <- function(x, name, ...) {
   if (!is.numeric(x)) return(render.categorical.default(x))
   what <- switch(name,
-                 Neonatal.mortality.rate = "Median [Min, Max]",
-                 Infant.mortality.rate = "Median [Min, Max]",
-                 Under.5.mortality.rate = "Median [Min, Max]",
-                 Maternal.mortality.rate = "Median [Min, Max]",
+                 Neonatal.mortality.rate = "Median [Min,Max]",
+                 Infant.mortality.rate = "Median [Min,Max]",
+                 Under.5.mortality.rate = "Median [Min,Max]",
+                 Maternal.mortality.rate = "Median [Min,Max]",
                  GDP = "Mean (SD)",
                  popdens = "Mean (SD)",
                  urban = "Mean (SD)",
@@ -54,47 +54,49 @@ rndr <- function(x, name, ...) {
 
 
 
-levels(data$armed.conflict) <- c("No Armed Conflict","Armed Conflict")
-# labels <- list(
-#   variable = list(
-#   GDP = "GDP per capita",
-#   OECD = "OECD member" ,
-#   popdens = "Population density",
-#   urban = "Urban residence",
-#   agedep = "Age dependency ratio",
-#   male_edu = "Male education",
-#   temp = "Temperature",
-#   Earthquake = "Earthquakes",
-#   Drought = "Droughts",
-#   Neonatal.mortality.rate= "Neonatal mortality rate per 1,000 live births",
-#   Infant.mortality.rate = "Infant mortality rate per 1,000 live births",
-#   Under.5.mortality.rate = "Under-5 mortality rate per 1,000 live births",
-#   Maternal.mortality.rate = "Maternal mortality ratio per 100,000 live births"),
-#   groups=list("","Armed Conflicted"))
+levels(data$armed.conflict) <- c("No","Yes")
+labels <- list(
+  variables = list(
+  GDP = "GDP per capita",
+  OECD = "OECD member" ,
+  popdens = "Population density",
+  urban = "Urban residence",
+  agedep = "Age dependency ratio",
+  male_edu = "Male education",
+  temp = "Temperature",
+  Earthquake = "Earthquakes",
+  Drought = "Droughts",
+  Neonatal.mortality.rate= "Neonatal mortality rate per 1,000 live births",
+  Infant.mortality.rate = "Infant mortality rate per 1,000 live births",
+  Under.5.mortality.rate = "Under-5 mortality rate per 1,000 live births",
+  Maternal.mortality.rate = "Maternal mortality ratio per 100,000 live births"),
+  groups=list("","Armed Conflicted"))
 
-label(data$GDP) = "GDP per capita"
-label(data$OECD) = "OECD member" 
-label(data$popdens) = "Population density"
-label(data$urban) = "Urban residence"
-label(data$agedep) = "Age dependency ratio"
-label(data$male_edu) = "Male education"
-label(data$temp) = "Temperature"
-label(data$Earthquake) = "Earthquakes"
-label(data$Drought) = "Droughts"
-label(data$Neonatal.mortality.rate)= "Neonatal mortality rate per 1,000 live births"
-label(data$Infant.mortality.rate) = "Infant mortality rate per 1,000 live births"
-label(data$Under.5.mortality.rate) = "Under-5 mortality rate per 1,000 live births"
-label(data$Maternal.mortality.rate) = "Maternal mortality ratio per 100,000 live births"
+# label(data$GDP) = "GDP per capita"
+# label(data$OECD) = "OECD member" 
+# label(data$popdens) = "Population density"
+# label(data$urban) = "Urban residence"
+# label(data$agedep) = "Age dependency ratio"
+# label(data$male_edu) = "Male education"
+# label(data$temp) = "Temperature"
+# label(data$Earthquake) = "Earthquakes"
+# label(data$Drought) = "Droughts"
+# label(data$Neonatal.mortality.rate)= "Neonatal mortality rate per 1,000 live births"
+# label(data$Infant.mortality.rate) = "Infant mortality rate per 1,000 live births"
+# label(data$Under.5.mortality.rate) = "Under-5 mortality rate per 1,000 live births"
+# label(data$Maternal.mortality.rate) = "Maternal mortality ratio per 100,000 live births"
 
-# strata <- c(list(Total=data), split(data, data$armed.conflict))
+strata <- c(list(Total=data), split(data, data$armed.conflict))
 
-table1(~ GDP+OECD+popdens+urban+agedep+male_edu+temp+Drought+Earthquake+Infant.mortality.rate+Under.5.mortality.rate+Neonatal.mortality.rate+Maternal.mortality.rate|armed.conflict, 
-       data=data,overall=c(left="Total"),caption = "Table 1. Demographic Table",
-     render = rndr,)
+# TABLE <- table1(~ GDP+OECD+popdens+urban+agedep+male_edu+temp+Drought+Earthquake+Infant.mortality.rate+Under.5.mortality.rate+Neonatal.mortality.rate+Maternal.mortality.rate|armed.conflict, 
+#        data=data,overall=c(left="Total"),caption = "Table 1. Demographic Table",
+#      render = rndr)
+
+#t1kable(TABLE) %>% add_header_above(c(" "," ", "Armed Conflict" = 2))
 # 
-# table1(strata,labels, groupspan=c(1, 2),
-#        render.continuous=my.render.cont, render.categorical=my.render.cat)
-# 
+table1(strata,labels, groupspan=c(1, 2),
+       render = rndr,caption = "Table 1. Demographic Table")
+
 
 
 
